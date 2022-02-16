15333264f021c66ad14c8b6793986022ed9b413f
Olexsii <realcarter50@gmail.com>
2022-02-16 12:38:16 +0200

    first commit

---
+++ GitHub Followers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHub Followers
+//
+//  Created by Olexsii Levchenko on 2/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
