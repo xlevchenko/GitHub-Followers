05ce9ba9f9eb8633da606d13a146205a9d41d12a
Olexsii <realcarter50@gmail.com>
2022-02-22 22:26:04 +0200

    Create Network Call - Get Followers

--- GitHub Followers/Screens/FollowerListVC.swift
+++ GitHub Followers/Screens/FollowerListVC.swift
@@ -14,7 +14,14 @@ class FollowerListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-       
+        NetworkManager.shared.getFollower(for: username, page: 1) { followers, errorMessage in
+            guard let followers = followers else {
+                self.presentAlertOnMainTread(title: "Bad Staff Happend", message: errorMessage!, buttonTitle: "Ok")
+                return
+            }
+            print("Followers.count = \(followers.count)")
+            print(followers)
+        }
         
     }
     override func viewWillAppear(_ animated: Bool) {
