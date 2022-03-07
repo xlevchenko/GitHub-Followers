596a168d31a08c21e42e5b4ee1b0a3a1cb90a6c7
Olexsii <realcarter50@gmail.com>
2022-03-07 22:33:33 +0200

    UITableView - Favorites Cell

---
+++ GitHub Followers/Custom Views/Cells/FavoriteCell.swift
@@ -0,0 +1,23 @@
+//
+//  FavoriteCell.swift
+//  GitHub Followers
+//
+//  Created by Olexsii Levchenko on 3/7/22.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
